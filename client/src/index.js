8212ee86c880cc47ba440c3421eeea428a976ff6
Nikki Johnson <nikkijohnson@Nikkis-MacBook-Air.local>
2019-01-07 20:54:24 -0700

    added redux stuff

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,15 @@ import App from './App';
 import { BrowserRouter, } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, 
   document.getElementById('root')
 );
 
